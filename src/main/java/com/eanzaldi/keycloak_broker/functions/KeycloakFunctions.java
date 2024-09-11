6478dbb484924c931db1e8658de3628040a8dce3
StefanoFioreSpindox <stefano.fiore@spindox.it>
2024-09-11 14:36:49 +0200

    jackson

--- src/main/java/com/eanzaldi/keycloak_broker/functions/KeycloakFunctions.java
+++ src/main/java/com/eanzaldi/keycloak_broker/functions/KeycloakFunctions.java
@@ -1,5 +1,9 @@
 package com.eanzaldi.keycloak_broker.functions;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.keycloak.events.Event;
+import org.keycloak.events.admin.AdminEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +20,15 @@ public class KeycloakFunctions {
     public Function<String, String> event() {
         return message -> {
             log.info("Received message headers: {}", message);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                Event event = objectMapper.readValue(message, Event.class);
+                log.info("Event parsed: {}", event);
+            } catch (JsonProcessingException e) {
+                log.error("Error parsing message");
+                throw new RuntimeException("Error parsing message: {}", e);
+            }
             return message;
         };
     }
@@ -23,7 +36,15 @@ public class KeycloakFunctions {
     @Bean
     public Function<String, String> adminEvent() {
         return message -> {
-            log.info("Received admin message headers: {}", message);
+            log.info("Received ADMIN message headers: {}", message);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                AdminEvent event = objectMapper.readValue(message, AdminEvent.class);
+                log.info("Event parsed: {}", event);
+            } catch (JsonProcessingException e) {
+                log.error("Error parsing message");
+                throw new RuntimeException("Error parsing message: {}", e);
+            }
             return message;
         };
     }
--- src/main/java/com/eanzaldi/keycloak_broker/functions/dto/KeycloakEventDto.java
+++
@@ -1,9 +0,0 @@
-package com.eanzaldi.keycloak_broker.functions.dto;
-
-import lombok.Data;
-
-@Data
-public class KeycloakEventDto {
-    private String userId;
-    private String type;
-}
