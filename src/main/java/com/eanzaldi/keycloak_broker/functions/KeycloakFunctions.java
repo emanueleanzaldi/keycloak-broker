d327ab5520f5deaff60244dc6bc93111c776de3f
StefanoFioreSpindox <stefano.fiore@spindox.it>
2024-09-11 15:15:39 +0200

    jackson

--- src/main/java/com/eanzaldi/keycloak_broker/functions/KeycloakFunctions.java
+++ src/main/java/com/eanzaldi/keycloak_broker/functions/KeycloakFunctions.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 import java.util.function.Function;
 
@@ -17,19 +19,35 @@ public class KeycloakFunctions {
     private static final Logger log = LoggerFactory.getLogger(KeycloakFunctions.class);
 
     @Bean
-    public Function<String, String> event() {
+    public Function<Message<String>, Message<String>> event() {
         return message -> {
-            log.info("Received message headers: {}", message);
+            log.info("Received message: {}", message.getPayload());
 
             ObjectMapper objectMapper = new ObjectMapper();
             try {
-                Event event = objectMapper.readValue(message, Event.class);
-                log.info("Event parsed: {}", event);
+                Event event = objectMapper.readValue(message.getPayload(), Event.class);
+                String targetTopic;
+                switch (event.getType()) {
+                    case REGISTER:
+                        targetTopic = "user-register-event";
+                        break;
+                    case LOGIN:
+                        targetTopic = "user-login-event";
+                        break;
+                    default:
+                        log.warn("Unhandled event type: {}", event.getType());
+                        return message;  // Restituisci il messaggio senza fare nulla se non è gestito
+                }
+
+                // Crea un nuovo messaggio con la destinazione impostata nell'header
+                return MessageBuilder.withPayload(message.getPayload())
+                        .setHeader("spring.cloud.stream.sendto.destination", targetTopic)
+                        .build();
+
             } catch (JsonProcessingException e) {
-                log.error("Error parsing message");
-                throw new RuntimeException("Error parsing message: {}", e);
+                log.error("Error parsing message: {}", e.getMessage());
+                throw new RuntimeException("Error parsing message", e);
             }
-            return message;
         };
     }
 
@@ -40,7 +58,7 @@ public class KeycloakFunctions {
             ObjectMapper objectMapper = new ObjectMapper();
             try {
                 AdminEvent event = objectMapper.readValue(message, AdminEvent.class);
-                log.info("Event parsed: {}", event);
+                log.info("Event ADMIN parsed: {}", event);
             } catch (JsonProcessingException e) {
                 log.error("Error parsing message");
                 throw new RuntimeException("Error parsing message: {}", e);
